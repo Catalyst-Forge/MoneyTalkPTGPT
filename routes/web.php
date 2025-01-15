07bc42265b058c7e4abfaee300084227a5cc8831
BagasAuliaAlfasyam <bagas.190180087@mhs.unimal.ac.id>
2025-01-15 18:08:33 +0700

    fix routes

--- routes/web.php
+++ routes/web.php
@@ -26,27 +26,30 @@ Route::get('/', function () {
 
 Auth::routes(['middleware' => ['redirectIfAuthenticated']]);
 
-
 Route::middleware(['auth', 'role.admin'])->group(function () {
     Route::get('/admin', [AdminController::class, 'index'])->name('admin.dashboard');
 
     Route::resource('categories', CategoriesController::class);
-    Route::resource('cashs', CashsController::class)->except(['show'])->names([
-        'index' => 'cashs.index',
-        'create' => 'cashs.create',
-        'store' => 'cashs.store',
-        'edit' => 'cashs.edit',
-        'update' => 'cashs.update',
-        'destroy' => 'cashs.destroy',
-    ]);
-    Route::resource('cashs-out', CashsOutController::class)->except(['show'])->names([
-        'index' => 'cashs-out.index',
-        'create' => 'cashs-out.create',
-        'store' => 'cashs-out.store',
-        'edit' => 'cashs-out.edit',
-        'update' => 'cashs-out.update',
-        'destroy' => 'cashs-out.destroy',
-    ]);
+    Route::resource('cashs', CashsController::class)
+        ->except(['show'])
+        ->names([
+            'index' => 'cashs.index',
+            'create' => 'cashs.create',
+            'store' => 'cashs.store',
+            'edit' => 'cashs.edit',
+            'update' => 'cashs.update',
+            'destroy' => 'cashs.destroy',
+        ]);
+    Route::resource('cashs-out', CashsOutController::class)
+        ->except(['show'])
+        ->names([
+            'index' => 'cashs-out.index',
+            'create' => 'cashs-out.create',
+            'store' => 'cashs-out.store',
+            'edit' => 'cashs-out.edit',
+            'update' => 'cashs-out.update',
+            'destroy' => 'cashs-out.destroy',
+        ]);
     Route::get('cashs/export-excel', [CashsController::class, 'exportExcel'])->name('cashs.exportExcel');
     Route::get('cashs/export-pdf', [CashsController::class, 'exportPDF'])->name('cashs.exportPdf');
     Route::get('cashsout/export-excel', [CashsOutController::class, 'exportExcel'])->name('cashsout.exportExcel');
@@ -64,29 +67,31 @@ Route::middleware(['auth', 'role.admin'])->group(function () {
     Route::get('/cash-out/monthly-report', [CashsOutController::class, 'monthlyReport'])->name('cashOut.monthlyReport');
 });
 
-
 Route::middleware(['auth', 'role.user'])->group(function () {
     Route::get('/home', [UserController::class, 'index'])->name('home');
 
     Route::resource('categories-owner', CategoriesController::class);
-    Route::resource('cashs-owner', CashsController::class)->except(['show'])->names([
-        'index' => 'cashs.index.owner',
-        'create' => 'cashs.create.owner',
-        'store' => 'cashs.store.owner',
-        'edit' => 'cashs.edit.owner',
-        'update' => 'cashs.update.owner',
-        'destroy' => 'cashs.destroy.owner',
-    ]);
-    Route::resource('cashs-out-owner', CashsOutController::class)->except(['show'])->names([
-        'index' => 'cashs-out.index.owner',
-        'create' => 'cashs-out.create.owner',
-        'store' => 'cashs-out.store.owner',
-        'edit' => 'cashs-out.edit.owner',
-        'update' => 'cashs-out.update.owner',
-        'destroy' => 'cashs-out.destroy.owner',
-    ]);
-
-    Route::resource('asset', AssetsController::class)->except(['show']);
+    Route::resource('cashs-owner', CashsController::class)
+        ->except(['show'])
+        ->names([
+            'index' => 'cashs.index.owner',
+            'create' => 'cashs.create.owner',
+            'store' => 'cashs.store.owner',
+            'edit' => 'cashs.edit.owner',
+            'update' => 'cashs.update.owner',
+            'destroy' => 'cashs.destroy.owner',
+        ]);
+    Route::resource('cashs-out-owner', CashsOutController::class)
+        ->except(['show'])
+        ->names([
+            'index' => 'cashs-out.index.owner',
+            'create' => 'cashs-out.create.owner',
+            'store' => 'cashs-out.store.owner',
+            'edit' => 'cashs-out.edit.owner',
+            'update' => 'cashs-out.update.owner',
+            'destroy' => 'cashs-out.destroy.owner',
+        ]);
+    Route::resource('asset-owner', AssetsController::class)->except(['show']);
 
     Route::get('/profile-owner', [ProfileOwnerUpdateController::class, 'index'])->name('profile.index.owner');
     Route::put('/profile-owner/update', [ProfileOwnerUpdateController::class, 'update'])->name('profile.update.owner');
